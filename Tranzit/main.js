62c815b4d85f789535c66a5ed967d0c466c3d051
Krule6 <140613194+Krule6@users.noreply.github.com>
2025-11-07 04:28:50 +0100

    Update main.js

--- Tranzit/main.js
+++ Tranzit/main.js
@@ -1,10 +1,1 @@
 
-(function () {
-    function onReady() { document.body.classList.add('ready'); }
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', onReady);
-    } else {
-        onReady();
-    }
-})();
-
